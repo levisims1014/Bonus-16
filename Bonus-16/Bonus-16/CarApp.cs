796ae6bac85f12020b9f8cd2d672263a0e875fcd
levisims1014 <42224339+levisims1014@users.noreply.github.com>
2018-10-25 09:39:35 -0400

    Add files via upload

---
+++ Bonus-16/Bonus-16/CarApp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus16
+{
+    class CarApp
+    {
+        public static void BuildCarList()
+        {
+            List<Car> myCarList = new List<Car>();
+            int userInput = GetNumberOfCars();
+
+
+            for (int i = 1; i <= userInput; i++)
+            {
+                Console.WriteLine("What is the " + i + " make? ");
+                string make = Console.ReadLine();
+                Console.WriteLine("What is the model?");
+                string model = Console.ReadLine();
+                Console.WriteLine("What is the year?");
+                int year = int.Parse(Console.ReadLine());
+                Console.WriteLine("What is the price?");
+                double price = double.Parse(Console.ReadLine());
+
+                Car myCar = new Car();
+                myCar.Make = make;
+                myCar.Model = model;
+                myCar.Year = year;
+                myCar.Price = price;
+
+                myCarList.Add(myCar);
+
+            }
+
+            foreach (Car item in myCarList)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+
+        }
+
+        public static int GetNumberOfCars()
+        {
+            Console.WriteLine("Please enter number of cars you want:");
+            int userInput = int.Parse(Console.ReadLine());
+            return userInput;
+        }
+
+    }
+}
---
+++ Bonus-16/Bonus-16/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus16
+{ //number of cars
+  //details for each car
+  //print all cars
+
+    //1. new class Car
+    //2. string for make,model, int year, double for price
+    //field is private
+    //property is public
+    //3. default constructor
+    //4. another constructor that takes input for each of the 4 arguments
+    //5. override ToString method returning a formatted string with the car details
+    //6. class CarApp - creates a car object, and displays the car information
+    class Car
+    {
+        string make;
+        string model;
+        int year;
+        double price;
+
+        public string Make
+        {
+            get
+            {
+                return make;
+            }
+            set
+            {
+                make = value;
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                model = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                year = value;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+
+
+        public Car()
+        {
+
+        }
+
+        public Car(string CarMake, string carModel, int carYear, double carPrice)
+        {
+            make = CarMake;
+            model = carModel;
+            year = carYear;
+            price = carPrice;
+
+        }
+        public override string ToString()
+        {
+            return make + "\t" + model + "\t" + year + "\t" + price;
+        }
+    }
+}
